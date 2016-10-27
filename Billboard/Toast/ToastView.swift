d0a7f779b6462161a5716d7a055b2a932b4daf90
Steven Hepting <shepting@gmail.com>
2016-10-26 20:09:39 -0700

    Rename Toast to Billboard.

---
+++ Billboard/Toast/ToastView.swift
@@ -0,0 +1,61 @@
+//
+//  ToastView.swift
+//  Toast
+//
+//  Created by Steven Hepting on 9/10/16.
+//  Copyright © 2016 Hepting. All rights reserved.
+//
+
+import UIKit
+
+class ToastView: UIView {
+//    let defaultFrame = CGRect(x: 100, y: 50, width: 200, height: 100)
+
+    var stackView = UIStackView()
+    let spinner = UIActivityIndicatorView()
+    let mainLabel = UILabel()
+    let detailLabel = UILabel()
+    let image = UIImage()
+    let button = UIButton()
+}
+
+extension ToastView {
+
+    convenience init<T: Toastable>(message: T) {
+        self.init()
+
+//        self.frame = defaultFrame
+        print("Showing \(message.mainMessage())")
+        backgroundColor = .black
+        layer.cornerRadius = 4
+
+        // Main
+        mainLabel.textColor = .white
+        mainLabel.text = message.mainMessage()
+
+        // Detail
+        detailLabel.textColor = .lightGray
+        detailLabel.text = message.detailMessage()
+
+        // Stack View
+        stackView = UIStackView(arrangedSubviews: [mainLabel, detailLabel])
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.addArrangedSubview(mainLabel)
+        stackView.addArrangedSubview(detailLabel)
+        stackView.axis = .vertical
+        stackView.frame = self.bounds
+        addSubview(stackView)
+        stackView.spacing = 10
+        stackView.layoutMargins = UIEdgeInsetsMake(10, 10, 10, 10)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.fillSuperview()
+    }
+
+    override func didMoveToSuperview() {
+        translatesAutoresizingMaskIntoConstraints = false
+        constrainNearTop()
+    }
+
+}
+
+
---
+++ Billboard/Toast/Todo.md
@@ -0,0 +1,12 @@
+# Todo
+- get side view buttons to stack
+
+- add buttons for NSNotification
+- add button for NSError
+
+# Done
+- get note button to dismiss automatically
+- make NSNotification toastable
+- made side view
+- add detail label to toast view
+- add stack view to toast view
